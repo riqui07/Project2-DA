80dce989d4c174a1b2e215e4c901b57141124d6e
Nicholas-Oliveira <140977949+Nicholas-Oliveira@users.noreply.github.com>
2026-05-15 02:16:56 +0100

    implemented spilling algorithm

--- Code/data_structures/InterferenceMan.cpp
+++ Code/data_structures/InterferenceMan.cpp
@@ -11,6 +11,34 @@ void InterferenceMan::runBasic(int nReg) {
 }
 
 void InterferenceMan::runSpilling(int nReg, int maxSpills) {
+    if (runBasic(nReg,graph) != -1) return true;
+
+    vector<Web> spilledWebs;
+    Graph<Web> copy = graph;
+
+    for(int numSpills = 0; numSpills <= maxSpills; numSpills++){
+        Vertex<Web>* highestDegree = nullptr;
+        int currentHighest = -1;
+
+        for(auto v : copy.getVertexSet()){
+            int degree = (int)v->getAdj().size();
+            if (degree > currentHighest) {
+                currentHighest = degree;
+                highestDegree = v;
+            }
+        }
+
+        if(highestDegree == nullptr) break;
+
+        spilledWebs.push_back(highestDegree->getInfo());
+        copy.removeVertex(bestSpill->getInfo()); 
+
+        if(runBasic(nReg,copy) != -1){
+            spilledResult = spilledWebs; 
+            return true;
+        }
+    }
+    return false;
 }
 
 void InterferenceMan::runSplitting(int nReg, int maxSplits) {

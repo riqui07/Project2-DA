654bec0c3fa3d16657679bbd1efd7eb394ec530b
riqui07 <henriquereis023@gmail.com>
2026-05-16 17:05:57 +0100

    Implemented a much better testing interface (doesn't have outputs to compare yet)

--- Code/tests.cpp
+++ Code/tests.cpp
@@ -19,10 +19,10 @@ vector<string> parseLines(const string& filename)
     return res;
 }
 
-void rodar(){
+void rodar(const string& ran_filename, const string& reg_filename, const string& output_filename){
     Parser parser;
-    parser.parse("Input/generated_inputs/ranges/generated_ranges2.txt");
-    parser.parse("Input/generated_inputs/registers/generated_registers2.txt");
+    parser.parse("Input/ranges/" + ran_filename);
+    parser.parse("Input/registers/" + reg_filename);
 
     SpiderMan spiderman(parser.getLiveRanges());
     spiderman.buildWebs();
@@ -35,30 +35,30 @@ void rodar(){
     if (reg.algorithm == "basic") {
         int colors_used = interference_man.runBasic(reg.num_registers);
         if (colors_used != -1)
-            interference_man.outputResultsSuccess("maboy.txt");
+            interference_man.outputResultsSuccess(output_filename);
         else
-            interference_man.outputResultsFailure("maboy.txt");
+            interference_man.outputResultsFailure(output_filename);
 
     } else if (reg.algorithm == "spilling") {
         bool success = interference_man.runSpilling(reg.num_registers, reg.numeric_value);
         if (success)
-            interference_man.outputResultsSuccess("maboy.txt");
+            interference_man.outputResultsSuccess(output_filename);
         else
-            interference_man.outputResultsFailure("maboy.txt");
+            interference_man.outputResultsFailure(output_filename);
 
     } else if (reg.algorithm == "splitting") {
         bool success = interference_man.runSplitting(reg.num_registers, reg.numeric_value);
         if (success)
-            interference_man.outputResultsSuccess("maboy.txt");
+            interference_man.outputResultsSuccess(output_filename);
         else
-            interference_man.outputResultsFailure("maboy.txt");
+            interference_man.outputResultsFailure(output_filename);
     }
 }
 
-vector<string> validateOutput(const string& filename){
+vector<string> validateOutput(const string& gen_filename, const string& exp_filename){
     vector<string> incorrect_lines;
-    vector<string> output = parseLines("Output/" + filename);
-    vector<string> expected = parseLines("Output/generated_outputs/generated_output2.txt");
+    vector<string> output = parseLines("Output/" + gen_filename);
+    vector<string> expected = parseLines("GeneratedExpectedOutput/" + exp_filename);
 
     if (output.size() != expected.size()){incorrect_lines.push_back("Incorrect number of lines");}
 
@@ -70,16 +70,29 @@ vector<string> validateOutput(const string& filename){
 }
 
 int main() {
-    rodar();
-    vector<string> res = validateOutput("maboy.txt");
-    if (res.empty()) {
-        cout << "\033[32m" << "Correct!" << "\033[0m" << endl;
-    } else {
-        cout << "\033[31m" << "=======================================" << "\033[0m" << endl;
-        cout << "\033[31m" << "Incorrect!" << "\033[0m" << endl;
-        cout << "\033[31m" << "First Incorrect Line: " << res[0] << "\033[0m" << endl;
-        cout << "\033[31m" << "Should be: " << res[1] << "\033[0m" << endl;
-        cout << "\033[31m" << "=======================================" << "\033[0m" << endl;
+    vector<vector<string>> testes{
+        {"ranges1.txt", "registers2.txt", "allocation1.txt", "generated_allocation1.txt"},
+        {"ranges2.txt", "registers2.txt", "allocation2.txt", "generated_allocation2.txt"},
+        {"ranges3.txt", "registers2.txt", "allocation3.txt", "generated_allocation3.txt"},
+        {"ranges4.txt", "registers1.txt", "allocation4.txt", "generated_allocation4.txt"},
+        {"ranges5.txt", "registers1.txt", "allocation5.txt", "generated_allocation5.txt"},
+        {"ranges6.txt", "registers3.txt", "allocation6.txt", "generated_allocation6.txt"},
+    };
+
+    for (int i = 0; i < testes.size(); i++){
+        rodar(testes[i][0], testes[i][1], testes[i][2]);
+        vector<string> res = validateOutput(testes[i][2], testes[i][3]);
+
+        cout << "Test " << i + 1 << ":";
+        if (res.empty()) {
+            cout << "\033[32m" << " Correct!" << "\033[0m" << endl;
+        } else {
+            cout << "\033[31m" << " Incorrect!" << "\033[0m" << endl;
+            cout << "\033[31m" << "First Incorrect Line: " << res[0] << "\033[0m" << endl;
+            cout << "\033[31m" << "Should be: " << res[1] << "\033[0m" << endl;
+        }
     }
+
+
     return 0;
 }
